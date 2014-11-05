160421852a4f224f82409e732fac980193d3fc3a
ophchu <ophchu@gmail.com>
2014-11-05 21:44:20 +0200

    sdaf

---
+++ .worksheet/src/secondcourse.test.scala
@@ -0,0 +1,6 @@
+package secondcourse
+
+object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(58); 
+  val a = List(10,2,3);System.out.println("""a  : List[Int] = """ + $show(a ))}
+ 
+}
